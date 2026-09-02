84c30aae3aee086a01b887da02f33e4b9d4ff4b0
Onyx34 <fred.terral34@gmail.com>
2026-09-02 11:35:59 +0200

    first hello world program

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Learning-Rust
 My first steps in rust.
+
+To dive in I used the official documentation for Rust at :
+https://doc.rust-lang.org/stable/book/
---
+++ hello-world/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello world!");
+    // println is a macro, the ! is used
+    // In case of a function ! is not used
+}
+// usual main() as entry point of the application
